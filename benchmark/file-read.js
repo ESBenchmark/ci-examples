dcd7b2eb51f2bef4640385e8b66874058930c755
Kaciras <Kaciras@protonmail.com>
2024-07-14 12:53:08 +0800

    init

---
+++ benchmark/file-read.js
@@ -0,0 +1,10 @@
+import { readFileSync } from "node:fs";
+import { readFile } from "node:fs/promises";
+import { defineSuite } from "esbench";
+
+const filename = "pnpm-lock.yaml";
+
+export default defineSuite(scene => {
+	scene.bench("sync", () => readFileSync(filename));
+	scene.benchAsync("async", () => readFile(filename));
+});
